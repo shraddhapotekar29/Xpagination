219926dbc35a69369dfb3d44af6ee304d495ceed
shraddha potekar <shraddhapotekar762@gmail.com>
2024-09-13 14:23:36 +0530

    Table Pagination

--- xpagination/src/Pagination/PaginationTable.js
+++ xpagination/src/Pagination/PaginationTable.js
@@ -3,12 +3,15 @@ import style from "./Pagination.module.css"
 const PaginationTable=({tableData})=>{
     return(<div style={{marginLeft:"2rem",marginRight:"2rem"}}>
     <table border={"1px solid #d7dbdd"}  width="100%" className={style.table}>
+        <thead>
         <tr>
                 <th>ID</th>
                 <th>Name</th>
                 <th>Email</th>
                 <th>Role</th>
         </tr>
+        </thead>
+        <tbody>
         {tableData.map((table)=>(
         <tr key={table.id}>
         <td>{table.id}</td>
@@ -16,6 +19,7 @@ const PaginationTable=({tableData})=>{
         <td>{table.email}</td>
         <td>{table.role}</td>
         </tr>))}
+        </tbody>
     </table>
     </div>)
 }
